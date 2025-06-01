293e36931bcb6ef942e166d6d8a6299973dc3be1
Michael Fehrer <mtfehrer@gmail.com>
2025-06-01 15:58:28 -0400

    a

--- start_edge_container.sh
+++ start_edge_container.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-pwd
+source /opt/ros/melodic/setup.bash
+export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:/home/edgeslam/Examples/ROS
 ./build_ros.sh
 roscore &
 cd Examples/ROS/Edge_SLAM/
-rosrun Edge_SLAM RGBD ../../../Vocabulary/ORBvoc.txt ../../RGB-D/TUM2.yaml server < edge_input.txt
+rosrun Edge_SLAM RGBD ../../../Vocabulary/ORBvoc.txt ../../RGB-D/TUM2.yaml server < /home/edgeslam/edge_input.txt
--- start_mobile_container.sh
+++ start_mobile_container.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+source /opt/ros/melodic/setup.bash
+export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:/home/edgeslam/Examples/ROS
 ./build_ros.sh
 roscore &
 cd Examples/ROS/Edge_SLAM/
-rosrun Edge_SLAM RGBD ../../../Vocabulary/ORBvoc.txt ../../RGB-D/TUM2.yaml client < mobile_input.txt
+mkdir frames
+rosrun Edge_SLAM RGBD ../../../Vocabulary/ORBvoc.txt ../../RGB-D/TUM2.yaml client < /home/edgeslam/mobile_input.txt
