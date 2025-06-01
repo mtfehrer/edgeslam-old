6e9e08875920d437d3a0524d16c327048e78c2c6
Michael Fehrer <mtfehrer@gmail.com>
2025-06-01 19:34:25 -0400

    a

--- start_mobile_container.sh
+++ start_mobile_container.sh
@@ -2,7 +2,10 @@
 
 source /opt/ros/melodic/setup.bash
 export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:/home/edgeslam/Examples/ROS
-./build_ros.sh
+#check if build already happened
+if [ ! -f "/home/edgeslam/Examples/ROS/Edge_SLAM/RGBD" ]; then
+    ./build_ros.sh
+fi
 roscore &
 cd Examples/ROS/Edge_SLAM/
 mkdir frames
