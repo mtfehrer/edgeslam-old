d38a5b918dd60197e4ccbd03eb8ac4c0e46a1c92
Michael Fehrer <mtfehrer@gmail.com>
2025-06-04 16:01:39 -0400

    a

--- start_mobile_container.sh
+++ start_mobile_container.sh
@@ -15,7 +15,7 @@ if [ ! -f "/home/edgeslam/Examples/ROS/Edge_SLAM/RGBD" ]; then
 fi
 roscore &
 cd Examples/ROS/Edge_SLAM/
-if [ ! -d "/home/edgeslam/Examples/ROS/Edge_SLAM"]; then
+if [ ! -d "/home/edgeslam/Examples/ROS/Edge_SLAM" ]; then
     mkdir frames
 fi
 rosrun Edge_SLAM ZED ../../../Vocabulary/ORBvoc.txt ../../RGB-D/TUM2.yaml client < /home/edgeslam/mobile_input.txt
