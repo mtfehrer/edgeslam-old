a397e66929800e8c26912ec7195859981212e43b
Michael Fehrer <mtfehrer@gmail.com>
2025-06-06 20:10:36 -0400

    a

--- download_and_build_zed.sh
+++
@@ -1,8 +0,0 @@
-wget https://download.stereolabs.com/zedsdk/4.2/cu12/ubuntu22
-mkdir -p ./src
-cd ./src
-git clone --recursive https://github.com/stereolabs/zed-ros-wrapper.git
-cd ../
-rosdep install --from-paths src --ignore-src -r -y
-catkin_make -DCMAKE_BUILD_TYPE=Release
-source ./devel/setup.bash
--- start_mobile_container.sh
+++ start_mobile_container.sh
@@ -2,14 +2,6 @@
 
 source /opt/ros/melodic/setup.bash
 export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:/home/edgeslam/Examples/ROS
-#check if zed stuff exists
-if [ ! -d "/home/edgeslam/zed_stuff" ]; then
-    mkdir zed_stuff
-    cp download_and_build_zed.sh ./zed_stuff
-    cd zed_stuff
-    ./download_and_build_zed.sh
-    cd ..
-fi
 #check if build already happened
 if [ ! -f "/home/edgeslam/Examples/ROS/Edge_SLAM/RGBD" ]; then
     ./build_ros.sh
