dc32703571a246143bd09e90bffb118b7235870e
Michael Fehrer <112262472+mtfehrer@users.noreply.github.com>
2025-06-08 13:18:58 -0400

    a

--- start_mobile_container.sh
+++ start_mobile_container.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 source /opt/ros/melodic/setup.bash
+source /home/catkin_ws/devel/setup.bash
 export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:/home/edgeslam/Examples/ROS
 roscore &
 cd /home/edgeslam/Examples/ROS/Edge_SLAM/
-rosrun Edge_SLAM ZED ../../../Vocabulary/ORBvoc.txt ../../RGB-D/TUM2.yaml client < /home/edgeslam/mobile_input.txt
+rosrun Edge_SLAM ZED ../../../Vocabulary/ORBvoc.txt ../../RGB-D/TUM2.yaml client < /home/edgeslam/mobile_input.txt &
+sleep 10
+roslaunch zed_wrapper zed2.launch
