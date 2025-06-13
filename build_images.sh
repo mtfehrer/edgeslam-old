44c0920aceb8b65a56076a51b8295f929e7b79d3
Michael Fehrer <mtfehrer@gmail.com>
2025-06-13 14:55:01 -0400

    a

--- build_images.sh
+++ build_images.sh
@@ -1,14 +1,21 @@
 #Check if edgeslam-mobile image exists
 if ! docker image inspect edgeslam-mobile > /dev/null 2>&1; then
     echo "Image 'edgeslam-mobile' not found. Building..."
-    docker build -t edgeslam-mobile -f Dockerfile.mobile .
+    docker build -t edgeslam-mobile -f Dockerfile.mobile ./edgeslam/
 else
     echo "Image 'edgeslam-mobile' already exists."
 fi
-Check if edgeslam-edge image exists
+#Check if edgeslam-edge image exists
 if ! docker image inspect edgeslam-edge > /dev/null 2>&1; then
     echo "Image 'edgeslam-edge' not found. Building..."
-    docker build -t edgeslam-edge -f Dockerfile.edge .
+    docker build -t edgeslam-edge -f Dockerfile.edge ./edgeslam/
 else
     echo "Image 'edgeslam-edge' already exists."
+fi
+#Check if path-planner image exists
+if ! docker image inspect path-planner > /dev/null 2>&1; then
+    echo "Image 'path-planner' not found. Building..."
+    docker build -t path-planner -f Dockerfile ./path-planner/
+else
+    echo "Image 'path-planner' already exists."
 fi
