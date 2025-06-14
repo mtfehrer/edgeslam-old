fb395016a8ba56f6411f0429ec0091b1cfab29a2
Michael Fehrer <mtfehrer@gmail.com>
2025-06-14 11:13:48 -0400

    rename path-planner to planner

--- planner/plan_path.py
+++ planner/plan_path.py
@@ -1,14 +1,8 @@
-import pymavlink
+import math
 import time
 import csv
-import math
 
 VOXEL_SIZE = 1
-
-def get_target():
-    #pure pursuit
-    pass
-
 
 occupied_voxels = set()
 free_voxels = set()
@@ -21,7 +15,6 @@ def get_voxel(point):
     k = math.floor(z / sz)
     return (i, j, k)
 
-
 while (True):
     points = []
     with open('points.csv', 'r') as file:
@@ -31,5 +24,3 @@ while (True):
             points.append(point)
     occupied_voxels = set([get_voxel(p) for p in points])
     time.sleep(1)
-
-    #generate trajectory
